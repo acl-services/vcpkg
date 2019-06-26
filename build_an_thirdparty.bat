57b79c552fff44a1382ea3893d2019458970e2ee
John Zhu <john_zhu@acl.com>
2019-06-26 12:30:04 -0700

    remove extra quata

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -1,12 +1,13 @@
 CALL bootstrap-vcpkg.bat
 
-SET packages="zlib protobuf"
+SET packages=zlib protobuf
 
 SET VCPKG_DEFAULT_TRIPLET=x86-windows-mix
 
+echo vcpkg.exe install %packages%
 vcpkg.exe install %packages%
 
-IF "%BUILD_NUMBER%"=="" (SET   BUILD_NUMBER="100")
+IF "%BUILD_NUMBER%"=="" (SET   BUILD_NUMBER=100)
 SET PACKAGE_ID="14.0.%BUILD_NUMBER%"
 
 vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version=%PACKAGE_ID% %packages%
