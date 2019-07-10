3862e28908fb1df7f3d88b1eb06fb2421b96667f
John Zhu <john_zhu@acl.com>
2019-07-10 16:41:55 -0700

    Add boost json-spirit cypropp

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -1,13 +1,13 @@
-CALL bootstrap-vcpkg.bat
+IF NOT "%1" == "SKIP_VCPKG" CALL bootstrap-vcpkg.bat
 
-SET packages=zlib protobuf
+SET packages=boost-format zlib protobuf json-spirit cryptopp
 
 SET VCPKG_DEFAULT_TRIPLET=x86-windows-mix
 
 echo vcpkg.exe install %packages%
 vcpkg.exe install %packages%
 
-IF "%BUILD_NUMBER%"=="" (SET   BUILD_NUMBER=100)
-SET PACKAGE_ID="14.0.%BUILD_NUMBER%"
+REM bump version number when add/change setting
+SET PACKAGE_ID="14.0.8"
 
 vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version=%PACKAGE_ID% %packages%
--- triplets/x86-windows-mix.cmake
+++ triplets/x86-windows-mix.cmake
@@ -1,6 +1,6 @@
 set(VCPKG_PLATFORM_TOOLSET v141)
 set(VCPKG_TARGET_ARCHITECTURE x86)
-if(${PORT} MATCHES "protobuf")
+if(${PORT} MATCHES "cryptopp|protobuf|json_spirit|boost.*")
 	set(VCPKG_CRT_LINKAGE static)
 	set(VCPKG_LIBRARY_LINKAGE static)
 else()
