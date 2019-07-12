50c383bc096f42f14e559bed27ae04a44f45eaf5
John Zhu <john_zhu@acl.com>
2019-07-12 11:57:16 -0700

    Add icu

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -1,6 +1,6 @@
 IF NOT "%1" == "SKIP_VCPKG" CALL bootstrap-vcpkg.bat
 
-SET packages=boost-format zlib protobuf json-spirit cryptopp
+SET packages=boost-format zlib protobuf json-spirit cryptopp icu
 
 SET VCPKG_DEFAULT_TRIPLET=x86-windows-mix
 
