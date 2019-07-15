585b467b14793fec2ad2a3abf148f31efe9f9593
John Zhu <john_zhu@acl.com>
2019-07-15 09:56:12 -0700

    update build script for uploading

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -1,6 +1,6 @@
 IF NOT "%1" == "SKIP_VCPKG" CALL bootstrap-vcpkg.bat
 
-SET packages=boost-format zlib protobuf json-spirit cryptopp icu
+SET packages=minizip boost-format protobuf json-spirit cryptopp icu
 
 SET VCPKG_DEFAULT_TRIPLET=x86-windows-mix
 
@@ -8,6 +8,8 @@ echo vcpkg.exe install %packages%
 vcpkg.exe install %packages%
 
 REM bump version number when add/change setting
-SET PACKAGE_ID="14.0.8"
+SET PACKAGE_ID=14.0.8
 
-vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version=%PACKAGE_ID% %packages%
+vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version="%PACKAGE_ID%" %packages%
+
+IF NOT "%BUILD_NUMBER%" == "" aws s3 cp an-thirdparty.%PACKAGE_ID%.nupkg "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/an-thirdparty.%PACKAGE_ID%.nupkg"
