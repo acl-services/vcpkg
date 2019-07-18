ba95c9e4eaa87bb969e3f96c8e73db0bcdf52c5b
John Zhu <john_zhu@acl.com>
2019-07-18 16:28:47 -0700

    changed to dll

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -1,14 +1,31 @@
-IF NOT "%1" == "SKIP_VCPKG" CALL bootstrap-vcpkg.bat
+@ECHO OFF
+set SKIP_VCPKG="OFF"
+set CLEANUP="OFF"
+:Loop
+IF "%~1"=="" GOTO Continue
+IF  /I "%~1"=="SKIP_VCPKG" set SKIP_VCPKG="ON"
+IF  /I "%~1"=="CLEANUP" set CLEANUP="ON"
+SHIFT
+GOTO Loop
+
+:Continue
+@ECHO ON
+IF /I %SKIP_VCPKG% == "OFF" CALL bootstrap-vcpkg.bat
 
 SET packages=minizip boost-format protobuf json-spirit cryptopp icu zlib:x64-windows
 
 SET VCPKG_DEFAULT_TRIPLET=x86-windows-mix
 
+echo Cleanup is %CLEANUP% 
+IF %CLEANUP% == "ON" call vcpkg.exe remove --recurse %packages%
+
 echo vcpkg.exe install %packages%
 vcpkg.exe install %packages%
 
+del /q an-thirdparty.*.nupkg
+
 REM bump version number when add/change setting
-SET PACKAGE_ID=14.0.8
+SET PACKAGE_ID=14.0.9
 
 vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version="%PACKAGE_ID%" %packages%
 
--- triplets/x86-windows-mix.cmake
+++ triplets/x86-windows-mix.cmake
@@ -1,9 +1,9 @@
-set(VCPKG_PLATFORM_TOOLSET v141)
 set(VCPKG_TARGET_ARCHITECTURE x86)
-if(${PORT} MATCHES "zlib")
-	set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_PLATFORM_TOOLSET v141)
+
+set(VCPKG_CRT_LINKAGE dynamic)
+if(${PORT} MATCHES "zlib|icu|json-spirit")
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
-	set(VCPKG_CRT_LINKAGE static)
 	set(VCPKG_LIBRARY_LINKAGE static)
 endif()
