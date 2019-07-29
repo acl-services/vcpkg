53a04fb43b7d55f5c8dbc54a91d5108685cc6cae
John Zhu <john_zhu@acl.com>
2019-07-29 16:17:34 -0700

    fix the debug dlls

--- build_an_thirdparty.bat
+++ build_an_thirdparty.bat
@@ -32,6 +32,6 @@ vcpkg.exe export --nuget --nuget-id=an-thirdparty --nuget-version="%PACKAGE_ID%"
 IF NOT "%BUILD_NUMBER%" == "" (
 aws s3 cp an-thirdparty.%PACKAGE_ID%.nupkg "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/an-thirdparty.%PACKAGE_ID%.nupkg"
 aws s3 sync installed/x86-windows-mix/bin "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/dlls/"
-aws s3 sync installed/x86-windows-mix/debug "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/dlls/"
+aws s3 sync installed/x86-windows-mix/debug/bin "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/debug_dlls/"
 aws s3 sync installed/x64-windows/bin "s3://acl-rnd-artifacts/an_thirdpartylib/%git_local_branch%/x64-dlls/"
 )
